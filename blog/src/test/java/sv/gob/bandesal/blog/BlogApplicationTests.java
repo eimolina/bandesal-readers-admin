dc911fc4b97326bad79bf761ca50e8e6de0a273e
Isaac Molina <izaac.everaldo@gmail.com>
2023-02-09 11:14:49 -0600

    Initial commit: Adding project files for api an readers web application

---
+++ blog/src/test/java/sv/gob/bandesal/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package sv.gob.bandesal.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
